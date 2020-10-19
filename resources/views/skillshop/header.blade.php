bb7e01076141d86fa6996097030c8dd9a64d9332
chrish <chrishcodes@gmail.com>
2020-10-18 20:12:35 -0700

    added skillshop header

---
+++ resources/views/skillshop/header.blade.php
@@ -0,0 +1,4 @@
+
+<header class="ssheader flex justify-center items-center min-w-full bg-blue-600 p-6">
+    <img src="http://www.lightspeedvt.com/hosted/images/0b/0ec3d1100f459d911d377287caeb14/SkillShop_Logo.png" class="max-w-3xl" alt="LightSpeed VT Skill Shop Logo">
+</header>
