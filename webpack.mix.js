12326a11181cd6a0e91df049abc036bcbf77197c
Chris <crehall88@gmail.com>
2020-12-11 21:35:55 -0800

    added Pages/Skillshop mix file

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+    .js('resources/js/Pages/Skillshop/form.vue', 'public/js/Pages/Skillshop')
+    .js('resources/js/Pages/Skillshop/index.vue', 'public/js/Pages/Skillshop')
+
+
+.postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
     ])
     .postCss('resources/css/main.css', 'public/css')
